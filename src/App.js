7d20bbbd51265be3c845db14f4359f71f4496d03
AndresJalife <ajalife@fi.uba.ar>
2022-08-27 13:40:57 -0300

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import React from "react";
+import LandingPage from "./view/landingPage/LandingPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+return (
+    <div>
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/" element={<Header someClass={"opaque"} />}>
+                    <Route path="" element={<LandingPage />}/>
+                </Route> */}
+                <Route path="/" element={<LandingPage/>}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
-  );
+)
 }
 
 export default App;
---
+++ src/view/landingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./landingPage.css";
+
+export default function LandingPage(){
+    return (
+        <div>
+            Hello Warlds.
+        </div>
+    )
+}
