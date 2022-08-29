7608e6ec4bbc90c63cca94889125a1fb1dcf12b0
AndresJalife <ajalife@fi.uba.ar>
2022-08-29 18:09:51 -0300

    add carousel

---
+++ src/component/ImageSlider/ImageSlider.js
@@ -0,0 +1,58 @@
+// import $ from 'jquery';
+import Licence1 from '../../resources/licence-1.png';
+import Licence2 from '../../resources/licence-2.png';
+import Licence3 from '../../resources/licence-1.png';
+import "./ImageSlider.css"
+
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel variant="dark" className="slider-container" activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={Licence1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={Licence2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={Licence3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
--- src/view/subview/licencesSubView/LicencesSubView.js
+++ src/view/subview/licencesSubView/LicencesSubView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./LicencesSubView.css";
+import ImageSlider from "../../../component/ImageSlider/ImageSlider";
 
 export default function LicencesSubView(){
     return (
         <div className="licences-container">
             Licences
+            <ImageSlider></ImageSlider>
         </div>
     )
 }
