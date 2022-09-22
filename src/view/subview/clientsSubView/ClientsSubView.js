bb2fb3b952c79aa881017558713bfe7097fde0a0
AndresJalife <ajalife@fi.uba.ar>
2022-09-21 21:15:12 -0300

    finish with clients and lower sizes

--- src/view/subview/clientsSubView/ClientsSubView.js
+++ src/view/subview/clientsSubView/ClientsSubView.js
@@ -1,11 +1,18 @@
 import React from 'react';
 // import {Col, Container, Row} from "react-bootstrap";
 import "./ClientsSubView.css";
+import Clients from "../../../resources/Clientes/Clientes.png";
+import Title from "../../../resources/Clientes/clients_title.png";
 
 export default function ClientsSubView(){
     return (
         <div className="clients-container">
-            Clients
+            <div id="clients-title-container" className="black-shadow">
+                <img src={Title} id="clients-title-img" alt="clients-title"/>
+            </div>
+            <div id="clients-container" className="black-shadow">
+                <img src={Clients} id="clients-img" alt="clients"/>
+            </div>
         </div>
     )
 }
