46723fa714ece324d107887657e2ccd72eadb028
AndresJalife <ajalife@fi.uba.ar>
2022-10-05 00:41:18 -0300

    menu color

--- src/component/videoModal/VideoModal.js
+++ src/component/videoModal/VideoModal.js
@@ -2,22 +2,21 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Iframe from "../iFrame/IFrame";
 import './VideoModal.css';
-// import {MdCloseFullscreen} from "react-icons/md";
+import {AiOutlineClose} from "react-icons/ai";
 
 export default function VideoModal({show, setShow, videoUrl, _title, isMobile}) {
 
     function getCloseHeader() {
-        if (isMobile) {
-            // return  <div className="prueba">
-            //             <MdCloseFullscreen color={'white'} size={'100%'} onClick={onClose}/>
-            //         </div>
+        return  <div className="prueba">
+                    <AiOutlineClose color={'white'} size={'10%'} onClick={onClose}/>
+                </div>
             // return      <Modal.Header closeButton style={{backgroundColor: "#646464"}}/>
-        }
+
     }
 
-    // const onClose = () => {
-    //     setShow(false);
-    // }
+    const onClose = () => {
+        setShow(false);
+    }
 
     return (
             <Modal
